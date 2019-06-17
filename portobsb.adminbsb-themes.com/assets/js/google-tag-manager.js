a6ae0007cb0997d1e16c2438c855fda274b18e9d
harisaravanan7 <hari.16mc@kct.ac.in>
2019-06-17 18:58:09 +0530

    First Commit

---
+++ portobsb.adminbsb-themes.com/assets/js/google-tag-manager.js
@@ -0,0 +1,3 @@
+var trackingId='UA-30038099-8';addLoadEvent(loadTracking);function addLoadEvent(func){var oldonload=window.onload;if(typeof window.onload!=='function'){window.onload=func;}else{window.onload=function(){oldonload();func();};}}
+function loadTracking(){window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());gtag('config',trackingId);}
